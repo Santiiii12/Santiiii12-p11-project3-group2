a67474518aa3c873b34bbf75f66468512282b25f
chiaros93 <chiara_rossetti@outlook.it>
2023-10-13 09:48:34 +0200

    egfe

--- script.js
+++ script.js
@@ -7,24 +7,35 @@ button.addEventListener('click', (event) => {
     traerRickyMorty();
 })
 
-function buscar() {
-    
-}
-
 function traerRickyMorty() {
     const input = document.querySelector('input');
-    
+
     fetch(input.value)
-        .then((res) => res.json())
-        .then((data) => {
-
-            data.results.forEach(element => {
-                const json = JSON.stringify(element);
-                results.innerHTML = `<p>${json}</p>`;
-            });
-
+        .then((res) => {
+            if (res.status === 200) {
+              return res.json();
+            } else if (res.status === 404) {
+                results.innerHTML = '404 - Not Found';
+                throw new Error('404 - Not Found');
+            } else {
+                results.innerHTML = 'Errore ' + res.status;
+                throw new Error('Errore ' + res.status);
+            }
         })
-
+        .then((data) => {
+            results.innerHTML = '';
+            if (data.results.length === 0) {
+                results.innerHTML = '404 - Not Found';
+            } else {
+                data.results.forEach(element => {
+                    const json = JSON.stringify(element);
+                    results.innerHTML +=`<p>200 OK --- ${json}</p>`;
+                });
+            }
+        })
+        .catch((error) => {
+            results.innerHTML = `<p>${error.message}</p>`;
+        });
 }
 
 //traerRickyMorty();
